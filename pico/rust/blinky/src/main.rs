04e412dc7200ca364e364f9f3fbfab53cf5866d8
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2026-04-03 10:33:44 +0100

    feat: add pico onboard led blink rust code

--- pico/rust/blinky/src/main.rs
+++ pico/rust/blinky/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cyw43::aligned_bytes;
+use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::{bind_interrupts, dma};
+use embassy_time::{Duration, Timer};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"blinky"),
+    embassy_rp::binary_info::rp_program_description!(
+        c"This is a RP Pico 2 W's onboard LED code, connected via the WIFI cyw43 chip"
+    ),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+    DMA_IRQ_0 => dma::InterruptHandler<DMA_CH0>;
+});
+
+#[embassy_executor::task]
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, cyw43::SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>>,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    // Cast the raw address to the specific Aligned type expected by cyw43
+    // let fw = unsafe { &*(0x10100000 as *const cyw43::Aligned<cyw43::A4, [u8; 230321]>) };
+    // let clm = unsafe { &*(0x10140000 as *const cyw43::Aligned<cyw43::A4, [u8; 4752]>) };
+
+    // Load using aligned_bytes (This adds some time in compile if your project is large)
+    let fw = aligned_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm = aligned_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    let nvram = aligned_bytes!("../cyw43-firmware/nvram_rp2040.bin");
+    // picotool load ./cyw43-firmware/43439A0.bin -o 0x10100000
+    // picotool load ./cyw43-firmware/43439A0_clm.bin -o 0x10140000
+
+    let power = Output::new(p.PIN_23, Level::Low); // Power to WiFi chip
+    let chip_select = Output::new(p.PIN_25, Level::High); // Chip Select
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        RM2_CLOCK_DIVIDER,
+        pio.irq0,
+        chip_select,
+        p.PIN_24,
+        p.PIN_29,
+        dma::Channel::new(p.DMA_CH0, Irqs),
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, power, spi, fw, nvram).await;
+    spawner.spawn(unwrap!(cyw43_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let delay = Duration::from_millis(500);
+
+    loop {
+        info!("led on!");
+        control.gpio_set(0, true).await;
+        Timer::after(delay).await;
+
+        info!("led off!");
+        control.gpio_set(0, false).await;
+        Timer::after(delay).await;
+    }
 }
